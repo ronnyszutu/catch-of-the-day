4f0e695cb2c430310e5e7d99f68916c2c674893f
Ronny Szutu <ronnyszutu@gmail.com>
2017-05-19 17:37:43 -0400

    Update order state.

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		
 		// getinitialState
 		this.state = {
@@ -41,16 +42,26 @@ class App extends React.Component {
 		});
 	}
 
+	addToOrder(key) {
+		// take a copy of our state
+		const order = { ...this.state.order };
+		// update or add the new number of fish ordered
+		order[key] = order[key] + 1 || 1;
+		// update our state
+		this.setState({ order });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market" />
 					<ul className="list-of-fishes">
-						{ 
+						{ /* Never try to use key. React won't allow it. We must make a new prop. */ }  
+						{
 							Object
 								.keys(this.state.fishes) 
-								.map(key => <Fish key={ key } details={ this.state.fishes[key] } />)
+								.map(key => <Fish key={ key } index={ key } details={ this.state.fishes[key] } addToOrder={ this.addToOrder } />)
 						}
 					</ul>
 				</div>

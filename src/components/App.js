1aa7aea46c98e4ae9cbe45f0ab8546f02c5a3633
Ronny Szutu <ronnyszutu@gmail.com>
2017-05-19 18:15:45 -0400

    Display order state

--- src/components/App.js
+++ src/components/App.js
@@ -65,7 +65,8 @@ class App extends React.Component {
 						}
 					</ul>
 				</div>
-				<Order/>
+				{ /* Passing entire state is bad practice */ }
+				<Order fishes={ this.state.fishes } order={ this.state.order } />
 				<Inventory addFish={ this.addFish } loadSamples={ this.loadSamples } />
 			</div>
 		);

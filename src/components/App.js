571259df57a582cd49932473ab380773f860b821
Ronny Szutu <ronnyszutu@gmail.com>
2017-05-23 14:51:35 -0400

    Removing Items from State.

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,11 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
-		
+		this.removeFromOrder = this.removeFromOrder.bind(this);
+
 		// getinitialState
 		this.state = {
 			fishes: {
@@ -73,6 +75,13 @@ class App extends React.Component {
 		this.setState({ fishes });
 	}
 
+	removeFish(key) {
+		const fishes = { ...this.state.fishes };
+		// Set the fish to null instead of using 'delete' because of a limitation from Firebase
+		fishes[key] = null;
+		this.setState({ fishes });
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -85,6 +94,13 @@ class App extends React.Component {
 		// update or add the new number of fish ordered
 		order[key] = order[key] + 1 || 1;
 		// update our state
+		this.setState({ order });
+	}
+
+	removeFromOrder(key) {
+		const order = { ...this.state.order };
+		// Use 'delete' since Firebase isn't involved with orders
+		delete order[key];
 		this.setState({ order });
 	}
 
@@ -107,12 +123,14 @@ class App extends React.Component {
 					fishes={ this.state.fishes } 
 					order={ this.state.order } 
 					params={ this.props.params }
+					removeFromOrder={ this.removeFromOrder }
 				/>
 				<Inventory 
 					addFish={ this.addFish } 
 					loadSamples={ this.loadSamples } 
 					fishes={ this.state.fishes }
 					updateFish={ this.updateFish }
+					removeFish={ this.removeFish }
 				/>
 			</div>
 		);

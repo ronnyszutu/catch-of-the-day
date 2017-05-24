95ebcccf097e1c2ec8ee956845f7c3e88cd90ea4
Ronny Szutu <ronnyszutu@gmail.com>
2017-05-24 11:36:54 -0400

    Apparently I never added these files...

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyCkb6LMQPj8iz7BRiH8KtRRrgsnPqTcYSk",
+    authDomain: "catch-of-the-day-szuturon.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-szuturon.firebaseio.com",
+});
+
+export default base;
